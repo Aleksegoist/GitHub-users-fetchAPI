f37b3d7f52e8835988323a55ff8502a0882d580a
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-16 15:31:54 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Logo from './components/Logo';
+import Users from './routes/Users';
 
 function App() {
     return (
         <div className='min-h-screen bg-slate-900'>
             <div className='container text-gray-200 py-3'>
                 <Logo />
+                <Routes>
+                    <Route path='/' element={<Users />}></Route>
+                </Routes>
             </div>
         </div>
     );
---
+++ src/routes/Users.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    return <div>Users</div>;
+};
+
+export default Users;
